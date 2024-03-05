5c2012534adba5e1e19db7f049099b70dfa880b2
Anushko <anushkorc3102@gmail.com>
2024-03-05 22:16:48 +0530

    add

---
+++ MiniProject/src/test/java/com/assessment/MiniProject/MiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.MiniProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
